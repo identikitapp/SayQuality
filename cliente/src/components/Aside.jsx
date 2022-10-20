50e672afbb422c2162656c436a94355aa013d528
Zeqdev <Ezequiellopez.dev@gmail.com>
2022-10-20 16:47:05 -0300

    Fix: error en componente aside

--- cliente/src/components/Aside.jsx
+++ cliente/src/components/Aside.jsx
@@ -1,63 +1,78 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
-import { BiImageAdd } from 'react-icons/bi';
-import { BsFillPlayFill } from 'react-icons/bs';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react'
+import { BsFillPlayFill } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
 import createHeader from '../utils/createHeader'
 
-
 export const Aside = () => {
+	const [user, setUser] = useState(null)
+	const navigate = useNavigate()
 
-  const [user, setUser] = useState();
-  const navigate = useNavigate()
+	let handleClick0 = () => {
+		navigate('/perfil')
+	}
+	let handleClick1 = () => {
+		navigate('/cuestionario')
+	}
+	let handleClick2 = () => {
+		navigate('/ajustes')
+	}
+	const logout = () => {
+		localStorage.removeItem('token')
+		navigate('/acceder')
+	}
 
-    let handleClick0 = () => {
-      navigate("/perfil")
-    };
-    let handleClick1 = () => {
-      navigate("/cuestionario")
-    };
-    let handleClick2 = () => {
-      navigate("/ajustes")
-    };
-    const logout = () => {
-      localStorage.removeItem('token')
-      navigate("/acceder")
-    };
+	useEffect(() => {
+		const url = import.meta.env.VITE_URL_USER
 
-    useEffect(() => {
+		fetch(url, {
+			method: 'GET',
+			headers: createHeader(),
+		})
+			.then(response => response.json())
+			.then(result => {
+				if (!result.data) {
+					return Swal.fire({
+						icon: 'error',
+						title: result.error.message,
+						confirmButtonColor: '#0083bb',
+					})
+				}
 
-          const url = import.meta.env.VITE_URL_USER
+				return setUser(result.data.user)
+			})
+	}, [])
 
-          fetch(url, {
-              method: 'GET',
-              headers: createHeader(),
-          })
-              .then(response => {
-                console.log(response)
-        if (response.ok) {
-
-         return response.json()
-        } 
-      })
-              .then(data =>setUser(data.data.user))
-
-  }, [])
-
-  return (
-          <aside>
-            <div className='imagen_perfil'>
-                <p>Estudiante</p>
-                <img src={import.meta.env.VITE_URL_IMG + user.avatar}
-                alt={user.username} />
-            </div>
-            <div className='secciones'>
-                    <button className='enlaces_secciones' onClick={handleClick0}><BsFillPlayFill className='icon'/>Cursos</button>
-                    <button className='enlaces_secciones' onClick={handleClick1} ><BsFillPlayFill className='icon'/>Cuestionarios</button>
-                    <button className='enlaces_secciones' onClick={handleClick2}><BsFillPlayFill className='icon'/>Ajustes</button>
-                    <button className='enlaces_secciones' onClick={logout}><BsFillPlayFill className='icon'/>Cerrar Sesión</button>
-            </div>
-        </aside>
-
-  )
+	return (
+		<aside>
+			{user === null ? (
+				navigate('/acceder')
+			) : (
+				<>
+					<div className='imagen_perfil'>
+						<p>Estudiante</p>
+						<img src={import.meta.env.VITE_URL_IMG + user.avatar} alt={user.username} />
+					</div>
+					<div className='secciones'>
+						<button className='enlaces_secciones' onClick={handleClick0}>
+							<BsFillPlayFill className='icon' />
+							Cursos
+						</button>
+						<button className='enlaces_secciones' onClick={handleClick1}>
+							<BsFillPlayFill className='icon' />
+							Cuestionarios
+						</button>
+						<button className='enlaces_secciones' onClick={handleClick2}>
+							<BsFillPlayFill className='icon' />
+							Ajustes
+						</button>
+						<button className='enlaces_secciones' onClick={logout}>
+							<BsFillPlayFill className='icon' />
+							Cerrar Sesión
+						</button>
+					</div>
+				</>
+			)}
+		</aside>
+	)
 }
