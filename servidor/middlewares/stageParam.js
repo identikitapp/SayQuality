a399f7f9696b907eb52e3e2132721e59669d6c2a
Reymundus <53380484+soyReymundus@users.noreply.github.com>
2022-11-17 01:20:36 -0300

    Cambio menor
    - Arregle un bug al no ingresas una unidad en /files
    
    - Quite un console.log que deje para las pruebas

--- servidor/middlewares/stageParam.js
+++ servidor/middlewares/stageParam.js
@@ -3,6 +3,16 @@ const sql = require("../utils/sql.js");
 module.exports = function (req, res, next, stageID) {
     sql.GetStage(stageID)
         .then((stages) => {
+            if(stages.length == 0){
+                return res
+                .status(404)
+                .json({
+                    "error": {
+                        "message": "La unidad no existe."
+                    }
+                });
+            };
+
             req.paramStage = stages[0];
             next();
         })
